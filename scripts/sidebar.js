02f4aec858b5b26acb9b9b44713d3744a494c675
Sterophonick <sterophonick@gmail.com>
2022-05-12 21:54:28 -0600

    totally screwed with the layout

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,10 +1,10 @@
 document.write(`
-<div id="sidebar" class="column">
+<div class="column sidebar">
     <div class="header">
         <a href="./index.html" id="logo"><img id="pfp" src="./assets/img/global/pfp.png" title="look it's the boye! :3"></a>
         <h1>Sterophonick's Own<br>Little World</h1>
     </div>
-    <nav id="navigation">
+    <div id="navigation">
         <a href="projects.html"><img style="vertical-align:middle" src="./assets/img/global/folder.png"> Projects and Stuff</a><br />
         <a href="misc.html"><img style="vertical-align:middle" src="./assets/img/global/misc.png"> Miscellaneous Stuff</a><br />
         <a href="pages/projects/linuxnotes.html"><img style="vertical-align:middle" src="./assets/img/global/linuxgame.png"> Linux Gaming Notes</a><br />
@@ -13,12 +13,11 @@ document.write(`
         <a href="gallery.html"><img style="vertical-align:middle" src="./assets/img/global/gallery.png"> Gallery</a><br />
         <a href="greetz.html"><img style="vertical-align:middle" src="./assets/img/global/greetz.png"> Special Greetz</a><br />
         <a href="links.html"><img style="vertical-align:middle" src="./assets/img/global/links.png"> Links</a><br/>
-    </nav>
-    <div id="sidebar-bottom">
-        <a href="https://github.com/Sterophonick" title="GitHub" target="_blank"><img src="./assets/img/global/github.png" class="icon" /></a>
-        <a href="https://gbatemp.net/members/sterophonick.457315/" title="GBAtemp" target="_blank"><img src="./assets/img/global/tempy.webp" class="icon" /></a>
-        <a href="https://youtube.com/sterophonick" title="YouTube" target="_blank"><img src="./assets/img/global/youtube.webp" class="icon" /></a>
-        <a href="https://twitter.com/sterophonick" title="Twitter" target="_blank"><img src="./assets/img/global/twitter.png" class="icon" /></a><br/>
+
+        <a href="https://github.com/Sterophonick" title="GitHub" target="_blank"><img class="nav-icon" src="./assets/img/global/github.png" /></a>
+        <a href="https://gbatemp.net/members/sterophonick.457315/" title="GBAtemp" target="_blank"><img class="nav-icon" src="./assets/img/global/tempy.webp" /></a>
+        <a href="https://youtube.com/sterophonick" title="YouTube" target="_blank"><img class="nav-icon" src="./assets/img/global/youtube.webp" /></a>
+        <a href="https://twitter.com/sterophonick" title="Twitter" target="_blank"><img class="nav-icon" src="./assets/img/global/twitter.png" /></a><br/>
         <a href="https://github.com/Sterophonick/sterophonick.github.io" target="_blank"><small>Website Source</small></a>
     </div>
 </div>
