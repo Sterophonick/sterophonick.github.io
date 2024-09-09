7119866df5c5741c20f554bafc4092ef8303cc42
Atapi <34801996+Sterophonick@users.noreply.github.com>
2024-09-08 22:36:49 -0600

    major site rewrite!

---
+++ scripts/footer.js
@@ -0,0 +1,14 @@
+document.write(`
+<div id="header">
+    <div id="separator" style="margin-top:5px; margin-bottom: 5px;">
+        <img width="100%" height="6px" id="spacer" src="assets/img/global/border.png"">
+    </div>
+    <div id="message">
+        <p>
+            made with love 2020-2024 Atapi / Sterophonick<br/>
+            any and all mentions of properties not mine belong to their respective owners<br/>
+            <a href="https://github.com/Sterophonick/sterophonick.github.io" target="_blank"><small>Website Source</small></a>
+        </p>
+    </div>
+</div>
+`);
---
+++ scripts/header.js
@@ -0,0 +1,35 @@
+document.write(`
+<div id="header">
+    <div class="title" align="center">
+        <!-- the idea to use a table here came from Headspin. thanks for your old website about GBA compression, laddie  -->
+        <table id="titleTable" border="0" cellpadding="20px" cellspacing="5px" style="border-collapse: collapse" bordercolor="#111111" id="AutoNumber44">
+            <tr>
+            <td><a href="./index.html"><img id="logo" border="0" src="assets/img/global/pfp.png" width="128"></a></td>
+            <td>
+            <h1 style="margin-top:30px">Sterophonick's Own Little World</h1>
+            </tr>
+        </table>
+    </div>
+    <div id="nav" style="margin-top: -15px">
+        <a href="projects.html"><img class="nav-icon" src="./assets/img/global/folder.png"> Projects</a>
+        -
+        <a href="misc.html"><img class="nav-icon" src="./assets/img/global/misc.png"> Misc</a>
+        -
+        <a href="dumps.html"><img class="nav-icon" src="./assets/img/global/chip.png"> Dumps</a>
+        -
+        <a href="blog.html"><img class="nav-icon" src="./assets/img/global/notepad.png"> Blog</a>
+        -
+        <a href="gallery.html"><img class="nav-icon" src="./assets/img/global/gallery.png"> Gallery</a>
+        -
+        <a href="greetz.html"><img class="nav-icon" src="./assets/img/global/greetz.png"> Greetz</a>
+        -
+        <a href="http://users3.smartgb.com/g/g.php?a=s&i=g36-33432-a2"><img class="nav-icon" src="./assets/img/global/guestbook.png"> Guestbook!</a>
+        <br/>
+    </div>
+    <div id="separator" style="margin-top:5px; margin-bottom: 5px;">
+        <img width="100%" height="6px" id="spacer" src="assets/img/global/border.png"">
+        <br/>
+        <br/>
+    </div>
+</div>
+`);
