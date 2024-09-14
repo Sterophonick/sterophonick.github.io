66c8ff0670029b3dc42236f4e5f7a91f60ab1a82
Atapi <34801996+Sterophonick@users.noreply.github.com>
2024-09-13 22:11:41 -0600

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -345,6 +345,7 @@ var quoteStrings = [
     "if your grave doesnt say \"rest in peace\" on it you are automatically drafted into the skeleton war", //dril
     "the doritos corporation thanks you", // from kyle gospo at bazzite
     "Marcianito 100% real bailando cumbia.wmv", // Marcianito 100% real bailando cumbia.wmv
+    "Let's face it... I'm cute.", // :3
 ];
 
 function getQuoteTag() {
